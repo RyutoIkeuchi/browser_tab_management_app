298ebb0bc578b0e2f25080aea5a0548704ea8b1b
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-07-29 15:24:37 +0900

    🔧 RiverPodの導入

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,6 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final ogpDataFromInputUrlProvider = StateProvider((ref) {
+  return null;
+});
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
