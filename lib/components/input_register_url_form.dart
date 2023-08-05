ae773c1cba9d8b30b68f5485ad9ff0e71c9e1559
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-08-05 15:47:41 +0900

    ♻️ urlの入力フォームを別ファイルに分ける、また未入力時はエラー表示させる

---
+++ lib/components/input_register_url_form.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../provider/ogp_data.dart';
+
+class InputRegisterUrlForm extends ConsumerStatefulWidget {
+  const InputRegisterUrlForm({Key? key}) : super(key: key);
+
+  @override
+  _InputRegisterUrlFormState createState() => _InputRegisterUrlFormState();
+}
+
+class _InputRegisterUrlFormState extends ConsumerState<InputRegisterUrlForm> {
+  final TextEditingController _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  handleGetThumbnailFromWeb() {
+    if (_formKey.currentState!.validate()) {
+      ref.read(ogpDataProvider.notifier).getOgpData(_controller.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 800,
+            child: TextFormField(
+              controller: _controller,
+              maxLines: 1,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "URLが入力されてません";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  fillColor: Theme.of(context).cardColor,
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Theme.of(context).cardColor)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Theme.of(context).primaryColor)),
+                  labelText: "URLを入力"),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            width: 200,
+            child: ElevatedButton(
+                onPressed: handleGetThumbnailFromWeb,
+                child: const Text('サイトの情報を取得')),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:browser_tab_management_app/components/input_register_url_form.dart';
 import 'package:flutter/material.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,65 +8,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../components/preview_register_ogp_data.dart';
 import '../provider/ogp_data.dart';
 
-class RegisterPage extends ConsumerStatefulWidget {
-  const RegisterPage({Key? key}) : super(key: key);
-
-  @override
-  _RegisterPageState createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends ConsumerState<RegisterPage> {
-  final TextEditingController _controller = TextEditingController();
-
-  @override
-  void initState() {}
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    handleGetThumbnailFromWeb() {
-      ref.read(ogpDataProvider.notifier).getOgpData(_controller.text);
-    }
-
     return Expanded(
       child: ColoredBox(
           color: Theme.of(context).highlightColor,
           child: Padding(
             padding: const EdgeInsets.all(20),
             child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 800,
-                    child: TextField(
-                      controller: _controller,
-                      maxLines: 1,
-                      decoration: InputDecoration(
-                          fillColor: Theme.of(context).cardColor,
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).cardColor)),
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor)),
-                          labelText: "URLを入力"),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: 200,
-                    child: ElevatedButton(
-                        onPressed: handleGetThumbnailFromWeb,
-                        child: const Text('metaデータを取得')),
-                  ),
-                ],
-              ),
+              const InputRegisterUrlForm(),
               Container(
                 margin: const EdgeInsets.symmetric(vertical: 50),
                 child: const PreviewRegisterOgpData(),
