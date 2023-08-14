b7f0bd7c8ebf3a53c9083ab13e179760b6c14b33
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-08-14 12:10:10 +0900

    ✨ アビリティをクリックしたときに、その上にモーダルが表示されるようにkeyから座標データを取得する

--- lib/provider/ability_list_position.dart
+++ lib/provider/ability_list_position.dart
@@ -1,3 +1,6 @@
+import 'package:browser_tab_management_app/provider/widget_global_key.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final abilityListPositionProvider =
@@ -7,8 +10,25 @@ final abilityListPositionProvider =
 class AbilityListPositionNotifier extends StateNotifier<Map<String, dynamic>> {
   AbilityListPositionNotifier() : super(INITIAL_POSITION_DATA);
 
-  void toggleOpenModalStatus() {
-    final updateState = {...state, "is_open_modal": !state["is_open_modal"]};
+  void toggleOpenModalStatus(ref) {
+    final appBarGlobalKey = ref.watch(appBarGlobalKeyProvider);
+    RenderBox appBarBox =
+        appBarGlobalKey.currentContext!.findRenderObject() as RenderBox;
+
+    final navigationRailGlobalKey = ref.watch(navigationRailGlobalKeyProvider);
+    RenderBox navigationBox =
+        navigationRailGlobalKey.currentContext!.findRenderObject() as RenderBox;
+
+    final abilityGlobalKey = ref.watch(abilityWidgetGlobalKeyProvider);
+    RenderBox box =
+        abilityGlobalKey.currentContext!.findRenderObject() as RenderBox;
+    final position = box.localToGlobal(Offset.zero);
+    final updateState = {
+      "top": position.dy - appBarBox.size.height,
+      "left": position.dx - navigationBox.size.width,
+      "width": box.size.width,
+      "is_open_modal": !state["is_open_modal"]
+    };
     state = updateState;
   }
 }
