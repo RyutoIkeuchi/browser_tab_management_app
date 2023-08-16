f2274871ba373fa886e5261b0b89a092bc910dee
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-08-16 17:28:31 +0900

    💄 入力されたURLから作成されたプレビューデータの上側に保存ボタンとキャンセルボタンを配置

--- lib/components/preview_register_ogp_data.dart
+++ lib/components/preview_register_ogp_data.dart
@@ -26,104 +26,122 @@ class _PreviewRegisterOgpDataState
         error: (stack, err) => const Center(child: Text('エラーです')),
         data: (data) {
           return Container(
-            width: 600,
-            height: 500,
-            decoration:
-                BoxDecoration(color: Theme.of(context).cardColor, boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).shadowColor,
-                blurRadius: 2,
-              )
-            ]),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 250,
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(
+              width: 600,
+              height: 600,
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme.of(context).shadowColor,
+                    blurRadius: 2,
+                  )
+                ],
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: Theme.of(context).shadowColor))),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                                onPressed: () {}, icon: const Icon(Icons.close)),
+                            TextButton(onPressed: () {}, child: const Text('保存する'))
+                          ]),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 250,
+                      child: data.image != ""
+                          ? Image.network(data.image, fit: BoxFit.cover)
+                          : ColoredBox(
+                              color: Theme.of(context).highlightColor,
+                              child: const Center(child: Text('サムネイル')),
+                            ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 6),
+                              child: Text(
+                                data?.title,
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 6),
+                              child: Text(
+                                data?.description,
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Theme.of(context).disabledColor),
+                              ),
+                            ),
+                            Divider(
+                              thickness: 0.1,
+                              endIndent: 20,
                               color: Theme.of(context).shadowColor,
-                              width: 0.4))),
-                  child: data.image != ""
-                      ? Image.network(data.image, fit: BoxFit.cover)
-                      : ColoredBox(
-                          color: Theme.of(context).hintColor,
-                          child: const Center(child: Text('サムネイル')),
-                        ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 10),
+                              child: const Text(
+                                'アビリティ',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            OutlinedButton(
+                              key: abilityKey,
+                              onPressed: () {
+                                ref
+                                    .read(abilityModalPositionProvider.notifier)
+                                    .openModal(ref);
+                              },
+                              style: ButtonStyle(
+                                  side: MaterialStateProperty.all<BorderSide>(
+                                      BorderSide.none)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(6),
+                                child: Row(
+                                    children: DUMMY_ABILITY_LIST
+                                        .map((e) => Container(
+                                              margin: const EdgeInsets.only(
+                                                  right: 6),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 10,
+                                                      vertical: 5),
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(30),
+                                                  color: Theme.of(context)
+                                                      .focusColor),
+                                              child: Text(
+                                                e,
+                                                style: TextStyle(
+                                                    color: Theme.of(context)
+                                                        .hintColor),
+                                              ),
+                                            ))
+                                        .toList()),
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 6),
-                          child: Text(
-                            data?.title,
-                            style: const TextStyle(fontSize: 24),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 6),
-                          child: Text(
-                            data?.description,
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Theme.of(context).disabledColor),
-                          ),
-                        ),
-                        Divider(
-                          thickness: 0.1,
-                          endIndent: 20,
-                          color: Theme.of(context).shadowColor,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          child: const Text(
-                            'アビリティ',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                        OutlinedButton(
-                          key: abilityKey,
-                          onPressed: () {
-                            ref
-                                .read(abilityModalPositionProvider.notifier)
-                                .openModal(ref);
-                          },
-                          style: ButtonStyle(
-                              side: MaterialStateProperty.all<BorderSide>(
-                                  BorderSide.none)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(6),
-                            child: Row(
-                                children: DUMMY_ABILITY_LIST
-                                    .map((e) => Container(
-                                          margin:
-                                              const EdgeInsets.only(right: 6),
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 10, vertical: 5),
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(30),
-                                              color:
-                                                  Theme.of(context).focusColor),
-                                          child: Text(
-                                            e,
-                                            style: TextStyle(
-                                                color: Theme.of(context)
-                                                    .hintColor),
-                                          ),
-                                        ))
-                                    .toList()),
-                          ),
-                        ),
-                      ]),
-                ),
-              ],
-            ),
-          );
+              ));
         });
   }
 }
