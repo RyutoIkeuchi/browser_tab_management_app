5f1801e81f8cebeee6a2794fd2f343fbc2a82abf
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-08-22 21:06:33 +0900

    💄 ダッシュボード画面のTabのUIの修正とListからmap関数で一覧表示できるようにする

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -11,12 +11,24 @@ class _DashboardPageState extends State<DashboardPage>
     with SingleTickerProviderStateMixin {
   TabController? _tabController;
   int _currentIndex = 0;
-   final key = GlobalKey();
+  final key = GlobalKey();
+  final DUMMY_TAB_NAME_LIST = [
+    'Flutter',
+    'React',
+    'TypeScript',
+    'Python',
+    'Next.js',
+    'HTML',
+    'Go',
+    'Rust',
+    'Power BI'
+  ];
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(vsync: this, length: 3);
+    _tabController =
+        TabController(vsync: this, length: DUMMY_TAB_NAME_LIST.length);
     _tabController!.addListener(() {
       setState(() {
         _currentIndex = _tabController!.index;
@@ -42,134 +54,36 @@ class _DashboardPageState extends State<DashboardPage>
             width: double.infinity,
             child: TabBar(
               controller: _tabController,
-              indicatorColor: Colors.transparent, //タブ下部のline(インジケータ)の色を消す
-              tabs: [
-                _Tab(
-                  label: 'Flutter',
-                  index: 0,
-                  tabControllerIndex: _currentIndex,
-                ),
-                _Tab(
-                  label: 'Dart',
-                  index: 1,
-                  tabControllerIndex: _currentIndex,
-                ),
-                _Tab(
-                  label: 'Firebase',
-                  index: 2,
-                  tabControllerIndex: _currentIndex,
-                ),
-              ],
+              tabs: DUMMY_TAB_NAME_LIST.map<Widget>((d) {
+                return Tab(
+                  child: Center(
+                    child: Container(
+                      child: Text(
+                        d,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
             ),
           ),
-           Expanded(
-              child: Container(
-                color: Colors.blue.shade200,
-                child: TabBarView(
-                  key: key,
-                  controller: _tabController,
-                  children: <Widget>[
-                    UrlList(),
-                    _TabBarView(
-                      label: 'Dart',
-                      index: _currentIndex,
-                    ),
-                    _TabBarView(
-                      label: 'Firebase',
-                      index: _currentIndex,
-                    ),
-                  ],
-                ),
-              ),),
+          Expanded(
+            child: Container(
+              color: Colors.blue.shade200,
+              child: TabBarView(
+                key: key,
+                controller: _tabController,
+                children: DUMMY_TAB_NAME_LIST.map<Widget>((d) {
+                  return UrlList();
+                }).toList(),
+              ),
+            ),
+          ),
         ]),
-      ),
-    );
-  }
-}
-
-class _Tab extends StatelessWidget {
-  const _Tab({
-    Key? key,
-    required this.label,
-    required this.index,
-    required this.tabControllerIndex,
-  }) : super(key: key);
-
-  final String label;
-  final int index;
-  final int tabControllerIndex;
-
-  @override
-  Widget build(BuildContext context) {
-    return Tab(
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 11, vertical: 8),
-          decoration: BoxDecoration(
-            color:
-                index == tabControllerIndex ? Colors.blue : Colors.transparent,
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: Text(
-            label,
-            style: TextStyle(
-              color: index == tabControllerIndex ? Colors.white : Colors.grey,
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _TabBarView extends StatelessWidget {
-  const _TabBarView({
-    Key? key,
-    required this.label,
-    required this.index,
-  }) : super(key: key);
-
-  final String label;
-  final index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            label,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-            ),
-          ),
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'index:',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-              ),
-              const SizedBox(width: 10),
-              Text(
-                '${index.toString()}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
