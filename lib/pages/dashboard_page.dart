f432e7d47b83e42deb4b5a59e53fb4833affd9ca
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-08-23 21:02:20 +0900

    🚧 dashboard画面でURLの大分類を表示するためのカードウィジェットを作成中...

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -66,7 +66,57 @@ class _DashboardPageState extends State<DashboardPage>
       child: ColoredBox(
         color: Theme.of(context).primaryColorLight,
         child: Column(children: [
-          const SizedBox(height: 60),
+          const SizedBox(height: 40),
+          Container(
+            width: 200,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  offset: Offset(10, 10),
+                  blurRadius: 10.0,
+                  spreadRadius: 1.0,
+                ),
+              ],
+            ),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 40,
+                      ),
+                      Positioned(
+                        top: -15,
+                        left: 20,
+                        child: Container(
+                          width: 80,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(0, 5),
+                                blurRadius: 2.0,
+                                spreadRadius: 0,
+                              ),
+                            ],
+                          ),
+                          child: Icon(Icons.airplane_ticket),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text('ああああ'),
+                ]),
+          ),
           Container(
             height: 44,
             width: double.infinity,
