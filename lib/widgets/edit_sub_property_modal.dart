16109098935478e9f1ae87995eb5fa020eb1efa4
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-09-06 23:59:57 +0900

    🚧 サブプロパティ一覧をスクロールできるようにする

--- lib/widgets/edit_sub_property_modal.dart
+++ lib/widgets/edit_sub_property_modal.dart
@@ -28,88 +28,94 @@ class EditSubPropertyModal extends ConsumerWidget {
                 spreadRadius: 1.0,
               ),
             ]),
-            child: Column(
-              children: [
-                Container(
-                    height: 38,
-                    padding: const EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[100],
-                    ),
-                    child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: registrationURLData["sub_property_list"]
-                            .map<Widget>((e) => Container(
-                                margin: const EdgeInsets.only(right: 6),
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 5),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(30),
-                                    color: Theme.of(context).focusColor),
-                                child: Row(
+            child: Column(children: [
+              Container(
+                height: 60,
+                padding: const EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                ),
+                child: Column(children: [
+                  Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: registrationURLData["sub_property_list"]
+                          .map<Widget>((e) => Container(
+                              margin: const EdgeInsets.only(right: 6),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Theme.of(context).focusColor),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    e["name"],
+                                    style: TextStyle(
+                                        color: Theme.of(context).hintColor),
+                                  ),
+                                  IconButton(
+                                      padding: EdgeInsets.zero,
+                                      constraints: const BoxConstraints(),
+                                      color: Theme.of(context).hintColor,
+                                      iconSize: 16,
+                                      onPressed: () {
+                                        ref
+                                            .read(registrationURLDataProvider
+                                                .notifier)
+                                            .removeTargetSubProperty(e['id']);
+                                        ref
+                                            .read(subPropertyListProvider
+                                                .notifier)
+                                            .add(e);
+                                      },
+                                      icon: const Icon(Icons.close))
+                                ],
+                              )))
+                          .toList()),
+                  const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'オプションを選択するか、新しく作成する',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      )),
+                  Container(
+                    width: 20,
+                      child: ListView.builder(
+                        itemCount: subPropertyList.length,
+                        itemBuilder: (context, index) {
+                          return TextButton(
+                            onPressed: () {
+                              ref
+                                  .read(registrationURLDataProvider.notifier)
+                                  .addSubProperty(subPropertyList[index]);
+                              ref
+                                  .read(subPropertyListProvider.notifier)
+                                  .remove(subPropertyList[index]["id"]);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  top: 4, bottom: 4, left: 8, right: 8),
+                              child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    Text(
-                                      e["name"],
-                                      style: TextStyle(
-                                          color: Theme.of(context).hintColor),
+                                    Container(
+                                      margin: const EdgeInsets.only(right: 10),
+                                      child: const Icon(
+                                        Icons.subject,
+                                        size: 16,
+                                      ),
                                     ),
-                                    IconButton(
-                                        padding: EdgeInsets.zero,
-                                        constraints: const BoxConstraints(),
-                                        color: Theme.of(context).hintColor,
-                                        iconSize: 16,
-                                        onPressed: () {
-                                          ref
-                                              .read(registrationURLDataProvider
-                                                  .notifier)
-                                              .removeTargetSubProperty(e['id']);
-                                          ref
-                                              .read(
-                                                  subPropertyListProvider.notifier)
-                                              .add(e);
-                                        },
-                                        icon: const Icon(Icons.close))
-                                  ],
-                                )))
-                            .toList())),
-                const Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'オプションを選択するか、新しく作成する',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    )),
-                Column(
-                  children: subPropertyList.map((d) {
-                    return TextButton(
-                      onPressed: () {
-                        ref
-                            .read(registrationURLDataProvider.notifier)
-                            .addSubProperty(d);
-                        ref.read(subPropertyListProvider.notifier).remove(d["id"]);
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.only(
-                            top: 4, bottom: 4, left: 8, right: 8),
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.only(right: 10),
-                                child: const Icon(
-                                  Icons.subject,
-                                  size: 16,
-                                ),
-                              ),
-                              Text(d['name']),
-                            ]),
-                      ),
-                    );
-                  }).toList(),
-                )
-              ],
-            )));
+                                    Text(subPropertyList[index]['name']),
+                                  ]),
+                            ),
+                          );
+                        },
+                      ))
+                ]),
+              ),
+            ])));
   }
 }
